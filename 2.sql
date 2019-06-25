1a2ae700fe0a029a558902c99b2b16728a7c3987
wytay <40626621+wytay@users.noreply.github.com>
2019-06-25 19:58:28 +0500

    Add files via upload

---
+++ 2.sql
@@ -0,0 +1,23 @@
+--------------------------------------------------------
+--  DDL for Package PAYS_ACTIONS
+--------------------------------------------------------
+
+  CREATE OR REPLACE PACKAGE "pays_u"."PAYS_ACTIONS" AS 
+FUNCTION pays_create (	"P_TYPE" NUMBER,	"SUMM" NUMBER,	"AGENT_ID" NUMBER, 	"REC_ID" NUMBER 
+) RETURN NUMBER;
+  
+FUNCTION pays_update (	"ID" NUMBER, 
+	"CRE_DATE" TIMESTAMP:= NULL, 
+	"PAY_DATE" TIMESTAMP:=NULL, 
+	"STATUS" NUMBER:=NULL, 
+	"P_TYPE" NUMBER:=NULL, 
+	"SUMM" NUMBER:=NULL, 
+	"AGENT_ID" NUMBER:=NULL, 
+	"REC_ID" NUMBER:=NULL, 
+	"SEND_DATE" TIMESTAMP:=NULL
+) RETURN NUMBER;
+
+FUNCTION pays_delete (	"ID"  NUMBER ) RETURN NUMBER;
+END PAYS_ACTIONS;
+
+/
---
+++ 4.sql
@@ -0,0 +1,39 @@
+--------------------------------------------------------
+--  DDL for Procedure PAY_SEND
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE PROCEDURE "pays_u"."PAY_SEND" (ID_S IN NUMBER) AS
+BEGIN
+-- display the current system date in long format
+  DBMS_OUTPUT.PUT_LINE( 'SEND ' || TO_CHAR(SYSDATE, 'DL') || TO_CHAR(ID_S) );
+END pay_send;
+
+/
+
+--------------------------------------------------------
+--  DDL for Procedure FOR_SEND
+--------------------------------------------------------
+
+create or replace PROCEDURE          "FOR_SEND" 
+--(
+--  NUM_4_SEND IN NUMBER DEFAULT 1000 
+--)
+AS 
+BEGIN
+-- status=2 - оплачен 
+  FOR rec_row in (SELECT distinct rec_id FROM "pays_u".pays where status=2 ORDER BY dbms_random.random)
+  loop
+--     "pays_u".PAY_SEND(rec_row.rec_id);
+
+  FOR pay_row in (SELECT id_p FROM "pays_u".pays where status=2 and rec_id=rec_row.rec_id for update skip locked)
+  LOOP
+     "pays_u".PAY_SEND(pay_row.id_p);
+  END LOOP;
+  COMMIT;
+END LOOP;
+
+
+END FOR_SEND;
+
+/
