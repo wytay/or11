ffce403ebb9bd52e59521fa272a383652249d22b
wytay <40626621+wytay@users.noreply.github.com>
2019-06-25 21:52:05 +0500

    Add files via upload

--- 2.sql
+++ 2.sql
@@ -1,23 +1,26 @@
 --------------------------------------------------------
+--  File created - вторник-июня-25-2019   
+--------------------------------------------------------
+--------------------------------------------------------
 --  DDL for Package PAYS_ACTIONS
 --------------------------------------------------------
 
   CREATE OR REPLACE PACKAGE "pays_u"."PAYS_ACTIONS" AS 
-FUNCTION pays_create (	"P_TYPE" NUMBER,	"SUMM" NUMBER,	"AGENT_ID" NUMBER, 	"REC_ID" NUMBER 
-) RETURN NUMBER;
-  
-FUNCTION pays_update (	"ID" NUMBER, 
-	"CRE_DATE" TIMESTAMP:= NULL, 
-	"PAY_DATE" TIMESTAMP:=NULL, 
-	"STATUS" NUMBER:=NULL, 
-	"P_TYPE" NUMBER:=NULL, 
-	"SUMM" NUMBER:=NULL, 
-	"AGENT_ID" NUMBER:=NULL, 
-	"REC_ID" NUMBER:=NULL, 
-	"SEND_DATE" TIMESTAMP:=NULL
+FUNCTION pays_create (	"P_TYPE" IN "pays_u".pays.P_TYPE%TYPE,	"SUMM"  IN "pays_u".pays.SUMM%TYPE,	"AGENT_ID"  IN "pays_u".pays.AGENT_ID%TYPE, 	"REC_ID"  IN "pays_u".pays.REC_ID%TYPE 
 ) RETURN NUMBER;
 
-FUNCTION pays_delete (	"ID"  NUMBER ) RETURN NUMBER;
+FUNCTION pays_update (	"ID_P"  IN "pays_u".pays.ID_P%TYPE, 
+	"CRE_DATE"  IN "pays_u".pays.CRE_DATE%TYPE:= NULL, 
+	"PAY_DATE"  IN "pays_u".pays.PAY_DATE%TYPE:= NULL,
+	"STATUS"  IN "pays_u".pays.STATUS%TYPE:= NULL, 
+	"P_TYPE"  IN "pays_u".pays.P_TYPE%TYPE:= NULL,
+	"SUMM"  IN "pays_u".pays.SUMM%TYPE,
+    "AGENT_ID"  IN "pays_u".pays.AGENT_ID%TYPE,
+    "REC_ID"  IN "pays_u".pays.REC_ID%TYPE , 
+	"SEND_DATE" IN "pays_u".pays.SEND_DATE%TYPE 
+) RETURN NUMBER;
+
+FUNCTION pays_delete (	"ID_P"   IN "pays_u".pays.ID_P%TYPE) RETURN NUMBER;
 END PAYS_ACTIONS;
 
 /
--- 4.sql
+++ 4.sql
@@ -5,7 +5,7 @@ set define off;
 
   CREATE OR REPLACE PROCEDURE "pays_u"."PAY_SEND" (ID_S IN NUMBER) AS
 BEGIN
--- display the current system date in long format
+--dummy for send 1 pay record
   DBMS_OUTPUT.PUT_LINE( 'SEND ' || TO_CHAR(SYSDATE, 'DL') || TO_CHAR(ID_S) );
 END pay_send;
 
@@ -16,9 +16,7 @@ END pay_send;
 --------------------------------------------------------
 
 create or replace PROCEDURE          "FOR_SEND" 
---(
---  NUM_4_SEND IN NUMBER DEFAULT 1000 
---)
+-- select for sending per 1 record
 AS 
 BEGIN
 -- status=2 - оплачен 
@@ -37,3 +35,40 @@ END LOOP;
 END FOR_SEND;
 
 /
+create or replace PROCEDURE FOR_SEND1 AS 
+-- select for send many records
+BEGIN
+DECLARE
+l_cursor SYS_REFCURSOR;
+BEGIN
+FOR rec_row in (SELECT distinct rec_id FROM "pays_u".pays where status=2 ORDER BY dbms_random.random)
+  LOOP
+--     "pays_u".PAY_SEND(rec_row.rec_id);
+
+OPEN l_cursor FOR SELECT id_p FROM "pays_u".pays where status=2 and  "pays_u".pays.rec_id=rec_row.rec_id for update skip locked  ;
+"pays_u".pay_send1( l_cursor );
+CLOSE l_cursor;
+commit;
+ END LOOP;
+END;
+END FOR_SEND1;
+
+/
+
+
+create or replace PROCEDURE     pay_send1 (p_cursor IN SYS_REFCURSOR)
+AS
+--dummy for send many record
+
+ id1 NUMBER;
+BEGIN
+LOOP
+     FETCH p_cursor INTO id1;
+     EXIT WHEN p_cursor%NOTFOUND;
+
+-- process data as needed
+DBMS_OUTPUT.PUT_LINE( 'SENDed ' || TO_CHAR(SYSDATE, 'DL') || TO_CHAR(id1) );
+END LOOP;
+
+--CLOSE p_cursor;
+END;
